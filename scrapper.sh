3c6c9d832917011535e54a0bed14511c9c013d78
Charlie DELPLACE <delplacecharlie@gmail.com>
2025-04-04 16:16:17 +0000

    mise à jour scrapper.sh pour cron

--- scrapper.sh
+++ scrapper.sh
@@ -4,7 +4,7 @@
 url="https://forex.tradingsat.com/cours-euro-dollar-FX0000EURUSD/"
 
 # Fichier CSV où seront stockées les données
-output_file="prix_eur_usd.csv"
+output_file="/home/delplacecharlie/Projet-scrapping-CAC40/prix_eur_usd.csv"
 
 # Ajouter l'en-tête au fichier CSV si c'est la première exécution
 if [ ! -f "$output_file" ]; then
